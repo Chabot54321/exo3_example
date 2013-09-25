001c4dba31da223ad11c554dd407de479f5444e3
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-25 13:33:30 -0400

    add doc; format code

--- exo3_example/bin/exo3_example.dart
+++ exo3_example/bin/exo3_example.dart
@@ -1,16 +1,12 @@
 import 'package:exo3/exo3.dart';
 
-
 void main() {
-  
   double width = 10.0 ;
   double height = 20.0 ;
   double distance = 100.0 ;
   double time = 9.58 ;
   double area = air(width, height);
   double speed = vit(distance, time);
-
-  
   
   print('Ex. 2.1');
   print('si la largeur est 10 m et la longueur est 20 m, alors :');
